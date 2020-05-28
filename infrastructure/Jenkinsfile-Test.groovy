0fb6d0861d7fa3ae121721d24f28027652449476
Alex Lanz <alex@krumer.it>
2020-05-28 16:08:29 +0200

    Infrastructure folder

--- infrastructure/Jenkinsfile-Test.groovy
+++ infrastructure/Jenkinsfile-Test.groovy
@@ -6,7 +6,7 @@ pipeline {
     AWS_SECRET_ACCESS_KEY = credentials('AWS_SECRET_ACCESS_KEY')
     DOCKER_PROJECT_NAME = "odh-swaggerui"
     DOCKER_IMAGE = '755952719952.dkr.ecr.eu-west-1.amazonaws.com/odh-swaggerui'
-    DOCKER_TAG = "prod-$BUILD_NUMBER"
+    DOCKER_TAG = "test-$BUILD_NUMBER"
     SERVER_PORT = "1050"
 
     SWAGGER_JSON = "/code/default.yml"
@@ -35,8 +35,8 @@ pipeline {
       steps {
         sh '''
           aws ecr get-login --region eu-west-1 --no-include-email | bash
-          docker-compose --no-ansi -f docker-compose.build.yml build --pull
-          docker-compose --no-ansi -f docker-compose.build.yml push
+          docker-compose --no-ansi -f infrastructure/docker-compose.build.yml build --pull
+          docker-compose --no-ansi -f infrastructure/docker-compose.build.yml push
         '''
       }
     }
@@ -44,8 +44,8 @@ pipeline {
       steps {
         sshagent(['jenkins-ssh-key']) {
           sh """
-            ansible-galaxy install --force -r ansible/requirements.yml
-            ansible-playbook --limit=docker02.opendatahub.bz.it ansible/deploy.yml --extra-vars "build_number=${BUILD_NUMBER}"
+            (cd infrastructure/ansible && ansible-galaxy install -f -r requirements.yml)
+            (cd infrastructure/ansible && ansible-playbook --limit=test deploy.yml --extra-vars "release_name=${BUILD_NUMBER}")
           """
         }
       }
